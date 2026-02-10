f1ce2cce1657034d5613ff5213763dd0e239e223
root <root@CT101.home.local>
2026-02-10 19:17:15 +0000

    Fixing docker installation

--- install.sh
+++ install.sh
@@ -1,62 +1,66 @@
 #!/bin/bash
 
-# ====================================
-# Linux auto installer (Debian/Ubuntu)
-# Linux auto installer
-# Author : Tristan Divanach
-# ====================================
+# ==========================================
+#  Linux auto installer (Debian/Ubuntu)
+#  Author : Tristan Divanach
+# ==========================================
 
+mkdir -p logs
 LOG_FILE="logs/install.log"
 
-#Verify if you are root
 if [ "$EUID" -ne 0 ]; then
     echo "Please run this script as root (sudo command)." | tee -a $LOG_FILE
     exit 1
 fi
 
-echo "=====================================" | tee -a $LOG_FILE
-echo "Linux Auto Installer - Start of installation" | tee -a $LOG_FILE
-echo "=====================================" | tee -a $LOG_FILE
+echo "==========================================" | tee -a $LOG_FILE
+echo " Linux Auto Installer - Start of installation" | tee -a $LOG_FILE
+echo "==========================================" | tee -a $LOG_FILE
 
+# System update
 echo "System update..." | tee -a $LOG_FILE
 apt update && apt upgrade -y >> $LOG_FILE 2>&1
 
+# Installation of essentials tools
 echo "Installation of essentials tools..." | tee -a $LOG_FILE
-
 apt install -y git curl htop tree vim >> $LOG_FILE 2>&1
 
+# Firewall configuration
 echo "Firewall configuration..." | tee -a $LOG_FILE
 apt install -y ufw >> $LOG_FILE 2>&1
 ufw allow OpenSSH >> $LOG_FILE 2>&1
 ufw --force enable >> $LOG_FILE 2>&1
 
+# Fail2Ban installation
 echo "Fail2Ban installation..." | tee -a $LOG_FILE
 apt install -y fail2ban >> $LOG_FILE 2>&1
 systemctl enable fail2ban >> $LOG_FILE 2>&1
 systemctl start fail2ban >> $LOG_FILE 2>&1
 
-echo "Docker and docker-compose installation..." | tee -a $LOG_FILE
-apt install -y docker.io docker-compose >> $LOG_FILE 2>&1
-systemctl enable docker >> $LOG_FILE 2>&1
-systemctl start docker >> $LOG_FILE 2>&1
+# Docker installation
+echo "Docker installation..." | tee -a $LOG_FILE
+if [ -f "./docker-install.sh" ]; then
+    chmod +x ./docker-install.sh
+    ./docker-install.sh >> $LOG_FILE 2>&1
+else
+    echo "Error: ./docker-install.sh not found!" | tee -a $LOG_FILE
+fi
 
+# Creating it-tools container
 echo "Creating it-tools container..." | tee -a $LOG_FILE
 cat << EOF > docker-compose.yml
-services :
+services:
   it-tools:
     image: 'ghcr.io/corentinth/it-tools:latest'
     ports:
-        - '7474:80'
+      - '7474:80'
     restart: unless-stopped
     container_name: it-tools
     privileged: false
 EOF
 
+if command -v docker &> /dev/null; then
+    docker compose up -d >> $LOG_FILE 2>&1
+fi
+
 echo "Installation completed successfully" | tee -a $LOG_FILE
-echo "Log file : $LOG_FILE" | tee -a $LOG_FILE
-echo "=====================================" | tee -a $LOG_FILE
-
-
-
-
-
